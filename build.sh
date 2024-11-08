57da208888b40dfd32defab2aa4a6fd337a1153e
h3xit <connect@h3x.it>
2024-11-08 08:17:49 -0500

    update

--- build.sh
+++ build.sh
@@ -9,32 +9,36 @@ echo "===================================="
 echo "      Building docker images        "
 echo "===================================="
 
-echo "------------------------------------"
-echo " Building Reconh3x image            "
-echo "------------------------------------"
+
 
 cp setup.py ./src/docker/h3xrecon/
 cp -r src/h3xrecon ./src/docker/h3xrecon/
 
-docker buildx build --output type=docker --file ./src/docker/h3xrecon/Dockerfile --platform linux/amd64 --tag ghcr.io/h3xitsec/h3xrecon:latest ./src/docker/h3xrecon/
-
-rm -rf ./src/docker/h3xrecon/h3xrecon
-rm ./src/docker/h3xrecon/setup.py
-
+if [ -z "$GITHUB_ACTIONS" ]; then
 echo "------------------------------------"
-echo " Building Msg Broker                "
-echo "------------------------------------"
+    echo " Building Reconh3x image            "
+    echo "------------------------------------"
 
-docker buildx build --output type=docker --file ./src/docker/msgbroker/Dockerfile --platform linux/amd64 --tag ghcr.io/h3xitsec/h3xrecon_msgbroker:latest ./src/docker/msgbroker/
+    docker buildx build --output type=docker --file ./src/docker/h3xrecon/Dockerfile --platform linux/amd64 --tag ghcr.io/h3xitsec/h3xrecon:latest ./src/docker/h3xrecon/
 
-echo "------------------------------------"
-echo " Building Pgsql                     "
-echo "------------------------------------"
+    echo "------------------------------------"
+    echo " Building Msg Broker                "
+    echo "------------------------------------"
 
-cp src/h3xrecon/psql_dump.sql ./src/docker/pgsql/
-docker buildx build --output type=docker --file ./src/docker/pgsql/Dockerfile --platform linux/amd64 --tag ghcr.io/h3xitsec/h3xrecon_pgsql:latest ./src/docker/pgsql/
-rm ./src/docker/pgsql/psql_dump.sql
+    docker buildx build --output type=docker --file ./src/docker/msgbroker/Dockerfile --platform linux/amd64 --tag ghcr.io/h3xitsec/h3xrecon_msgbroker:latest ./src/docker/msgbroker/
 
-echo "===================================="
-echo " Docker build commands completed!   "
-echo "===================================="
+    echo "------------------------------------"
+    echo " Building Pgsql                     "
+    echo "------------------------------------"
+
+    cp src/h3xrecon/psql_dump.sql ./src/docker/pgsql/
+    docker buildx build --output type=docker --file ./src/docker/pgsql/Dockerfile --platform linux/amd64 --tag ghcr.io/h3xitsec/h3xrecon_pgsql:latest ./src/docker/pgsql/
+    rm ./src/docker/pgsql/psql_dump.sql
+
+    echo "===================================="
+    echo " Docker build commands completed!   "
+    echo "===================================="
+    
+    rm -rf ./src/docker/h3xrecon/h3xrecon
+    rm ./src/docker/h3xrecon/setup.py
+fi
