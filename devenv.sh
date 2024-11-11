617ef1a2f818ac4465be999bbeb83c40a7875214
h3xit <connect@h3x.it>
2024-11-11 12:48:08 -0500

    optimize ansible wrapper to list available playbooks and enable autocomplete

--- devenv.sh
+++ devenv.sh
@@ -31,5 +31,40 @@ export ANSIBLE_LOCAL_TEMP=./.ansible_tmp
 
 # Ansible Playbook wrapper
 apb() {
-    ansible-playbook ./src/ansible/"$1"
+    alias ls=ls
+    local playbook="$1"
+    if [[ -z "$playbook" ]]; then
+        # If no argument, list available playbooks
+        echo "Available Ansible playbooks:"
+        find ./src/ansible/ -maxdepth 1 -type f ! -name 'hosts.yaml' -name *.yaml  | xargs -n1 basename | cut -d. -f1
+        return 1
+    fi
+
+    # Auto-complete .yaml extension if not provided
+    if [[ ! "$playbook" == *.yaml ]]; then
+        playbook="${playbook}.yaml"
+    fi
+
+    # Check if playbook exists
+    if [[ ! -f "./src/ansible/$playbook" ]]; then
+        echo "Playbook not found: $playbook"
+        echo "Available Ansible playbooks:"
+        ls ./src/ansible/*.yaml | xargs -n1 basename
+        return 1
+    fi
+
+    ansible-playbook "./src/ansible/$playbook"
 }
+
+# Enable tab completion for Ansible playbooks
+_apb_completion() {
+    local cur playbooks
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    playbooks=$(ls ./src/ansible/*.yaml | xargs -n1 basename | sed 's/\.yaml$//')
+
+    COMPREPLY=( $(compgen -W "${playbooks}" -- "${cur}") )
+    return 0
+}
+
+complete -F _apb_completion apb
