461aab9542ad18fe55dd9ba4c64a7d5ca19f3f54
h3xit <h3xit@protonmail.com>
2024-11-13 12:32:04 -0500

    update to swarm deployment

--- localenv.sh
+++ localenv.sh
@@ -2,6 +2,24 @@
 
 export H3XRECON_BASE_DIRECTORY=$(pwd)
 
+# Source python virtual environment
+if [[ $VIRTUAL_ENV ]]; then
+    deactivate
+fi
+
+if [[ -f ${H3XRECON_BASE_DIRECTORY}/venv/bin/activate ]]; then
+    source ${H3XRECON_BASE_DIRECTORY}/venv/bin/activate
+else
+    # Check if python3-venv is installed
+    if ! dpkg -s python3-venv >/dev/null 2>&1; then
+        echo "python3-venv is not installed. Installing..."
+        sudo apt update && sudo apt install -y python3-venv
+    fi
+    python3 -m venv venv
+    source ${H3XRECON_BASE_DIRECTORY}/venv/bin/activate
+    pip install -r ${H3XRECON_BASE_DIRECTORY}/requirements.txt > /dev/null 2>&1
+fi
+
 # Environment variables
 ## reconh3x specific variables
 set -a && . ./.env.local && set +a
@@ -9,8 +27,6 @@ set -a && . ./.env.local && set +a
 # Add bin directory to PATH for local wrapper scripts
 export PATH="$PWD/bin:$PATH"
 
-# Set PYTHONPATH to src directory
-export PYTHONPATH="$PWD/src"
 # Set PostgreSQL environment for easy use of psql
 export PGHOST=$H3XRECON_DB_HOST
 export PGPORT=$H3XRECON_DB_PORT
@@ -22,44 +38,4 @@ export ANSIBLE_HOME=./ansible
 export ANSIBLE_INVENTORY=./ansible/hosts.yaml
 export ANSIBLE_LOCAL_TEMP=./.ansible_tmp
 
-alias h3xrecon='docker run --rm -it --network host --env-file ./.env.local ghcr.io/h3xitsec/h3xrecon_client'
-
-# Ansible Playbook wrapper
-# apb() {
-#     alias ls=ls
-#     local playbook="$1"
-#     if [[ -z "$playbook" ]]; then
-#         # If no argument, list available playbooks
-#         echo "Available Ansible playbooks:"
-#         find ./src/ansible/ -maxdepth 1 -type f ! -name 'hosts.yaml' -name *.yaml  | xargs -n1 basename | cut -d. -f1
-#         return 1
-#     fi
-
-#     # Auto-complete .yaml extension if not provided
-#     if [[ ! "$playbook" == *.yaml ]]; then
-#         playbook="${playbook}.yaml"
-#     fi
-
-#     # Check if playbook exists
-#     if [[ ! -f "./src/ansible/$playbook" ]]; then
-#         echo "Playbook not found: $playbook"
-#         echo "Available Ansible playbooks:"
-#         ls ./src/ansible/*.yaml | xargs -n1 basename
-#         return 1
-#     fi
-
-#     ansible-playbook "./src/ansible/$playbook"
-# }
-
-# # Enable tab completion for Ansible playbooks
-# _apb_completion() {
-#     local cur playbooks
-#     COMPREPLY=()
-#     cur="${COMP_WORDS[COMP_CWORD]}"
-#     playbooks=$(ls ./src/ansible/*.yaml | xargs -n1 basename | sed 's/\.yaml$//')
-
-#     COMPREPLY=( $(compgen -W "${playbooks}" -- "${cur}") )
-#     return 0
-# }
-
-# complete -F _apb_completion apb
+alias h3xrecon='docker run --rm -it --network host --env-file ${H3XRECON_BASE_DIRECTORY}/.env.local ghcr.io/h3xitsec/h3xrecon_client'
