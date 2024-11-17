d4af0bc7d992769309858f515da849108509f997
h3xit <h3xit@protonmail.com>
2024-11-17 01:54:37 -0500

    update

--- docker/msgbroker/entrypoint.sh
+++ docker/msgbroker/entrypoint.sh
@@ -3,11 +3,16 @@
 # Start NATS server in the background
 nats-server -js --store_dir=/data -m 8222 &
 
-# Wait for NATS to be ready
-sleep 2
+if [ -z $(which nats) ]; then
+    echo "installing nats cli"
+    wget https://github.com/nats-io/natscli/releases/download/v0.1.5/nats-0.1.5-linux-amd64.zip
+    unzip nats-0.1.5-linux-amd64.zip
+    mv nats-0.1.5-linux-amd64/nats /usr/local/bin/
+    rm -rf nats-0.1.5-linux-amd64*
+fi
 
 # Run init script once
-/init.sh
+sh /init.sh
 
 # Wait for NATS server to exit
 wait
--- docker/worker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PYTHONUNBUFFERED=1
-export PYTHONPATH=/app:$PYTHONPATH
-
-echo "Starting worker"
-/app/venv/bin/python -m h3xrecon_worker.main
