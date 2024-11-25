44339bf9753506c35f969244e54c294c582a5f6c
h3xit <h3xit@protonmail.com>
2024-11-24 22:44:47 -0500

    changed local env script

--- shell.nix
+++ shell.nix
@@ -33,9 +33,8 @@ pkgs.mkShellNoCC {
   shellHook = ''
     export PATH="$(pwd)/bin:~/.local/share/go/bin:/home/h3x/.pdtm/go/bin:$PATH"
     export LD_LIBRARY_PATH="/run/current-system/sw/share/nix-ld/lib"
-    export $(cat ./.env.local) > /dev/null 2>&1
+    source ./.env.local.sh
     alias h3xrecon="docker run --network=host --rm -it -v ~/.h3xrecon:/root/.h3xrecon ghcr.io/h3xitsec/h3xrecon_cli:latest"
-
   '';
 }
 
