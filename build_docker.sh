ad49bf5ac0408c8e10beb76c0edbf0d6c0ef500d
h3xit <h3xit@protonmail.com>
2024-11-24 20:55:18 -0500

    add dockerfile for client

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 python -m hatchling build && \
-docker build -t ghcr.io/h3xitsec/h3xrecon_database:dev -f ./Dockerfile.database . && \
-docker build -t ghcr.io/h3xitsec/h3xrecon_server:dev -f ./Dockerfile.server . && \
-docker build -t ghcr.io/h3xitsec/h3xrecon_worker:dev -f ./Dockerfile.worker .
+docker build -t ghcr.io/h3xitsec/h3xrecon/database:dev -f ./Dockerfile.database . && \
+docker build -t ghcr.io/h3xitsec/h3xrecon/server:dev -f ./Dockerfile.server . && \
+docker build -t ghcr.io/h3xitsec/h3xrecon/worker:dev -f ./Dockerfile.worker . && \
+docker build -t ghcr.io/h3xitsec/h3xrecon/client:dev -f ./Dockerfile.client . 
