9ca6fe32a7e2f3edaaf2678e3b8e8f72eaec90c1
h3xit <h3xit@protonmail.com>
2024-11-24 20:50:03 -0500

    update

--- src/h3xrecon/__init__.py
+++ src/h3xrecon/__init__.py
@@ -5,10 +5,12 @@
 from .core import Config
 from .server import JobProcessor, DataProcessor
 from .worker import Worker
+from .client import Client
 
 __all__ = [
     'Config',
     'JobProcessor',
     'DataProcessor',
     'Worker',
+    'Client',
 ]
---
+++ src/h3xrecon/client/__init__.py
@@ -0,0 +1,7 @@
+"""
+Command-line interface for H3XRecon.
+"""
+
+from h3xrecon.client.client import Client
+
+__all__ = ['Client']
