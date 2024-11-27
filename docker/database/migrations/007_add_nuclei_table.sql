3e33a97ffaf6b3604a2070f420f64f5de91756c6
h3xit <h3xit@protonmail.com>
2024-11-27 08:25:16 -0500

    SQL migration to create the nuclei table

---
+++ docker/database/migrations/007_add_nuclei_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS nuclei (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(1024) NOT NULL,
+    matched_at VARCHAR(1024) NOT NULL,
+    type VARCHAR(50),
+    ip VARCHAR(45),
+    port INTEGER,
+    template_path VARCHAR(255),
+    template_id VARCHAR(100),
+    template_name VARCHAR(255),
+    severity VARCHAR(20),
+    program_id INTEGER REFERENCES programs(id) ON DELETE CASCADE NOT NULL,
+    discovered_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
