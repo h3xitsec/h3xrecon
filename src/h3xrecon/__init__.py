3b649ea93677a7c1d3fc3e2b956a248c6ea2a12d
h3xit <85270067+h3xitsec@users.noreply.github.com>
2024-12-04 14:43:27 -0500

    Revert "Unit tests for nuclei, resolve_domain and reverse_resolve_ip (#74)" (#75)
    This reverts commit a7959141febd27f762b6c6be875fb57f9a80debe.

--- src/h3xrecon/__init__.py
+++ src/h3xrecon/__init__.py
@@ -1,13 +1,12 @@
-# # SPDX-FileCopyrightText: 2024-present h3xit <h3xit@protonmail.com>
-# #
-# # SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2024-present h3xit <h3xit@protonmail.com>
+#
+# SPDX-License-Identifier: MIT
 
-# from .server.jobprocessor import JobProcessor
-# from .server.dataprocessor import DataProcessor
-# from .worker.worker import Worker
+from .server import JobProcessor, DataProcessor
+from .worker import Worker
 
-# __all__ = [
-#     'JobProcessor',
-#     'DataProcessor',
-#     'Worker',
-# ]
+__all__ = [
+    'JobProcessor',
+    'DataProcessor',
+    'Worker',
+]
--- src/h3xrecon/plugins/plugins/nuclei.py
+++ src/h3xrecon/plugins/plugins/nuclei.py
@@ -2,35 +2,63 @@ from typing import AsyncGenerator, Dict, Any
 from h3xrecon.plugins import ReconPlugin
 from loguru import logger
 from dataclasses import dataclass, asdict, field
-from h3xrecon.plugins.helper import send_nuclei_data, send_ip_data, send_domain_data, send_service_data
-from urllib.parse import urlparse
 import asyncio
 import json
 import os
-from pydantic import BaseModel, Field, IPvAnyAddress, AnyHttpUrl, constr
-from typing import Union, Optional
 
 @dataclass
 class FunctionParams():
     target: str
     extra_params: list = field(default_factory=list)
 
-class FunctionOutput(BaseModel):
-    url: Union[AnyHttpUrl, str] = Field(pattern=r'^(https?://[^\s]+|\d+\.\d+\.\d+\.\d+:\d+)$')  # Validates HTTP(S) URL or IP:Port
-    matched_at: Union[AnyHttpUrl, str] = Field(pattern=r'^(https?://[^\s]+|\d+\.\d+\.\d+\.\d+:\d+)$')  # Validates HTTP(S) URL or IP:Port
-    type: str = Field(pattern='^(http|tcp|udp)$')  # Restricts to specific protocols
-    ip: IPvAnyAddress  # Validates IPv4 or IPv6 addresses
-    port: int = Field(ge=1, le=65535)  # Port range validation
-    scheme: Optional[str] = Field(default=None, pattern='^(http|https|ftp|ssh|tcp|udp)?$')  # Common schemes, optional
+@dataclass
+class FunctionOutput():
+    url: str
+    matched_at: str
+    type: str
+    ip: str
+    port: int
+    scheme: str
     template_path: str
     template_id: str
     template_name: str
-    severity: str = Field(pattern='^(info|low|medium|high|critical)$')  # Standard severity levels
+    severity: str
+
 
 class Nuclei(ReconPlugin):
     @property
     def name(self) -> str:
         return os.path.splitext(os.path.basename(__file__))[0]
+
+    @property
+    def sample_output(self) -> Dict[str, Any]:
+        output_data = {
+            "program_id": 245,
+            "execution_id": "e3de832a-418a-458d-82d5-28c8def3229d",
+            "source": {
+                "function": "nuclei",
+                "params": {
+                    "target": "example.com",
+                    "extra_params": ["-t", "http/technologies"]
+                },
+                "force": False
+            },
+            "output": {
+                "url": "https://example.com",
+                "matched_at": "https://example.com",
+                "type": "http",
+                "ip": "1.1.1.1",
+                "port": 443,
+                "scheme": "https",
+                "template_path": "http/technologies/sample-detect.yaml",
+                "template_id": "sample-detect",
+                "template_name": "sample cdn detection",
+                "severity": "info"
+            },
+            "timestamp": "2024-01-01T00:00:00+00:00"
+        }
+
+        return output_data
 
     async def execute(self, params: Dict[str, Any], program_id: int = None, execution_id: str = None) -> AsyncGenerator[Dict[str, Any], None]:
         function_params = asdict(FunctionParams(**params))
@@ -51,33 +79,34 @@ class Nuclei(ReconPlugin):
                 json_data = json.loads(output)
                 logger.debug(f"Nuclei output: {json_data}")
                 nuclei_output = FunctionOutput(
-                    url=json_data.get('url', ''),
-                    matched_at=json_data.get('matched-at', ''),
-                    type=json_data.get('type', ''),
-                    ip=json_data.get('ip', ''),
-                    port=json_data.get('port', 0),
-                    scheme=json_data.get('scheme', ''),
-                    template_path=json_data.get('template', ''),
-                    template_id=json_data.get('template-id', ''),
-                    template_name=json_data.get('info', {}).get('name', ''),
-                    severity=json_data.get('info', {}).get('severity', 'info')
+                    url=json_data.get('url', {}),
+                    matched_at=json_data.get('matched-at', {}),
+                    type=json_data.get('type', {}),
+                    ip=json_data.get('ip', {}),
+                    port=json_data.get('port', {}),
+                    scheme=json_data.get('scheme', ""),
+                    template_path=json_data.get('template', {}),
+                    template_id=json_data.get('template-id', {}),
+                    template_name=json_data.get('info', {}).get('name', {}),
+                    severity=json_data.get('info', {}).get('severity', {})
                 )
-                yield nuclei_output.model_dump()
-            except Exception as e:
-                logger.error(f"Error processing Nuclei output: {e}")
-                yield {}
+                yield asdict(nuclei_output)
+            except json.JSONDecodeError as e:
+                logger.error(f"Failed to parse JSON output: {e}")
 
         await process.wait()
     
     async def process_output(self, output_msg: Dict[str, Any], db = None) -> Dict[str, Any]:
+        from h3xrecon.plugins.helper import send_nuclei_data, send_ip_data, send_domain_data, send_service_data
+        from urllib.parse import urlparse
         if output_msg.get('in_scope', False):
-            if output_msg.get('output', {}).get('type', "") == "http":
-                hostname = urlparse(output_msg.get('output', {}).get('url', "")).hostname            
+            if output_msg.get('output').get('type') == "http":
+                hostname = urlparse(output_msg.get('output').get('url')).hostname            
             else:
-                hostname = output_msg.get('output', {}).get('url', "").split(":")[0]
+                hostname = output_msg.get('output').get('url').split(":")[0]
             await send_domain_data(data=hostname, program_id=output_msg.get('program_id'))
-            await send_ip_data(data=output_msg.get('output', {}).get('ip', ""), program_id=output_msg.get('program_id'))
-            await send_nuclei_data(data=output_msg.get('output', {}), program_id=output_msg.get('program_id'))
+            await send_ip_data(data=output_msg.get('output').get('ip'), program_id=output_msg.get('program_id'))
+            await send_nuclei_data(data=output_msg.get('output'), program_id=output_msg.get('program_id'))
             # Find scheme and protocol
 
             if output_msg.get('output').get('type') == "http":
