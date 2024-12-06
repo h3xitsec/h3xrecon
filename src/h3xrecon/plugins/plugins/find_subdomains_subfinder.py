1b12d505917fb63e1885362bf7a8dbf991b34ca5
h3xit <h3xit@protonmail.com>
2024-12-06 08:20:59 -0500

    fixed plugin's sending of data

--- src/h3xrecon/plugins/plugins/find_subdomains_subfinder.py
+++ src/h3xrecon/plugins/plugins/find_subdomains_subfinder.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator, Dict, Any
 from h3xrecon.plugins import ReconPlugin
 from h3xrecon.core import *
+from h3xrecon.plugins.helper import send_domain_data
 from loguru import logger
 import asyncio
 import os
@@ -30,12 +31,4 @@ class FindSubdomainsSubfinder(ReconPlugin):
         logger.info(f"Finished {self.name} on {params.get("target", {})}")
     
     async def process_output(self, output_msg: Dict[str, Any], db = None) -> Dict[str, Any]:
-        self.config = Config()
-        self.qm = QueueManager(self.config.nats)
-        domain_msg = {
-            "program_id": output_msg.get('program_id'),
-            "data_type": "domain",
-            "in_scope": output_msg.get('in_scope'),
-            "data": output_msg.get('output', {}).get('subdomain')
-        }
-        await self.qm.publish_message(subject="recon.data", stream="RECON_DATA", message=domain_msg)
+        await send_domain_data(data=output_msg.get('output', {}).get('subdomain'), program_id=output_msg.get('program_id'))
