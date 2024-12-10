db62943bbd2eaddbf940117e0349e13ffa6d170b
h3xit <h3xit@protonmail.com>
2024-12-09 22:51:36 -0500

    new test plugin to busy a worker

---
+++ src/h3xrecon/plugins/plugins/sleep.py
@@ -0,0 +1,32 @@
+from typing import AsyncGenerator, Dict, Any
+from h3xrecon.plugins import ReconPlugin
+from h3xrecon.core import *
+from loguru import logger
+import asyncio
+import json
+import os
+
+class Sleep(ReconPlugin):
+    @property
+    def name(self) -> str:
+        return os.path.splitext(os.path.basename(__file__))[0]
+
+    async def execute(self, params: Dict[str, Any], program_id: int = None, execution_id: str = None, ) -> AsyncGenerator[Dict[str, Any], None]:
+        logger.info(f"Running {self.name} on {params.get("target", {})}")
+        command = f"sleep 45"
+        logger.debug(f"Running command: {command}")
+        process = await asyncio.create_subprocess_shell(
+            command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            shell=True
+        )
+        
+        async for output in self._read_subprocess_output(process):
+            yield ""
+
+
+        await process.wait()
+
+    async def process_output(self, output_msg: Dict[str, Any], db = None, qm = None) -> Dict[str, Any]:    
+        pass
