714756f60287c88975e55ee39340617d702512fa
h3xit <85270067+h3xitsec@users.noreply.github.com>
2024-12-16 14:16:17 -0500

    control channel and killjob commands last bit (#127)

--- docker/msgbroker/entrypoint.sh
+++ docker/msgbroker/entrypoint.sh
@@ -5,10 +5,27 @@ nats-server -js --store_dir=/data -m 8222 &
 
 if [ -z $(which nats) ]; then
     echo "installing nats cli"
-    wget https://github.com/nats-io/natscli/releases/download/v0.1.5/nats-0.1.5-linux-amd64.zip
-    unzip nats-0.1.5-linux-amd64.zip
-    mv nats-0.1.5-linux-amd64/nats /usr/local/bin/
-    rm -rf nats-0.1.5-linux-amd64*
+    attempts=0
+    max_attempts=3
+    success=false
+
+    while [ $attempts -lt $max_attempts ] && [ "$success" = false ]; do
+        if wget https://github.com/nats-io/natscli/releases/download/v0.1.5/nats-0.1.5-linux-amd64.zip && \
+           unzip nats-0.1.5-linux-amd64.zip && \
+           mv nats-0.1.5-linux-amd64/nats /usr/local/bin/ && \
+           rm -rf nats-0.1.5-linux-amd64*; then
+            success=true
+        else
+            attempts=$((attempts + 1))
+            if [ $attempts -lt $max_attempts ]; then
+                echo "Attempt $attempts failed. Retrying in 10 seconds..."
+                sleep 10
+            else
+                echo "Failed to install nats cli after $max_attempts attempts"
+                exit 1
+            fi
+        fi
+    done
 fi
 
 # Run init script once
