866dd96a40518d4c2386e378be4cd191427bc68e
h3xit <h3xit@protonmail.com>
2025-01-04 21:20:54 -0500

    migration to websites and websites_table

---
+++ docker/database/migrations/016_add_websites_table.sql
@@ -0,0 +1,98 @@
+CREATE TABLE IF NOT EXISTS websites (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(1024) UNIQUE NOT NULL,
+    host VARCHAR(255) DEFAULT NULL,
+    port INTEGER DEFAULT NULL,
+    scheme VARCHAR(50) DEFAULT NULL,
+    techs TEXT[] DEFAULT NULL,
+    program_id INTEGER REFERENCES programs(id) ON DELETE CASCADE NOT NULL,
+    discovered_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
+);
+
+ALTER TABLE websites ADD CONSTRAINT unique_websites_program_id_url UNIQUE (program_id, url);
+
+INSERT INTO websites (url, port, scheme, techs, program_id, discovered_at, host)
+SELECT 
+    url,
+    port,
+    scheme,
+    tech,
+    program_id,
+    discovered_at,
+    REGEXP_REPLACE(REGEXP_REPLACE(url, '^https?://([^/]+).*$', '\1'), ':[0-9]+$', '') as host
+FROM urls;
+
+CREATE TABLE IF NOT EXISTS websites_paths (
+    id SERIAL PRIMARY KEY,
+    website_id INTEGER REFERENCES websites(id) ON DELETE CASCADE NOT NULL,
+    path character varying(1024) DEFAULT NULL::character varying,
+    final_path character varying(1024) DEFAULT NULL::character varying,
+    port integer,
+    tech text[],
+    response_time character varying(50) DEFAULT NULL::character varying,
+    lines integer,
+    title character varying(1024) DEFAULT NULL::character varying,
+    words integer,
+    method character varying(10) DEFAULT NULL::character varying,
+    scheme character varying(50) DEFAULT NULL::character varying,
+    status_code integer,
+    content_type character varying(255) DEFAULT NULL::character varying,
+    content_length integer,
+    chain_status_codes integer[],
+    page_type character varying(50),
+    body_preview text,
+    response_hash character varying(255)
+);
+
+
+ALTER TABLE websites_paths ADD CONSTRAINT unique_websites_paths_website_id_path UNIQUE (website_id, path);
+
+
+INSERT INTO websites_paths (website_id, path, final_path, port, tech, response_time, lines, title, words, method, scheme, status_code, content_type, content_length, chain_status_codes, page_type, body_preview)
+SELECT 
+    w.id as website_id,
+    CASE 
+        WHEN u.url ~ '^https?://[^/]+/?$' THEN '/'
+        ELSE REGEXP_REPLACE(u.url, '^https?://[^/]+', '')
+    END as path,
+    REGEXP_REPLACE(u.final_url, '^https?://[^/]+', '') as final_path,
+    u.port,
+    u.tech,
+    u.response_time,
+    u.lines,
+    u.title,
+    u.words,
+    u.method,
+    u.scheme,
+    u.status_code,
+    u.content_type,
+    u.content_length,
+    u.chain_status_codes,
+    u.page_type,
+    u.body_preview
+FROM urls u
+JOIN websites w ON u.url = w.url;
+
+INSERT INTO websites_paths (website_id, path, port, tech, response_time, lines, title, words, method, scheme, status_code, content_type, content_length, page_type, body_preview)
+SELECT 
+    w.id as website_id,
+    CASE 
+        WHEN u.final_url ~ '^https?://[^/]+/?$' THEN '/'
+        ELSE REGEXP_REPLACE(u.final_url, '^https?://[^/]+', '')
+    END as path,
+    u.port,
+    u.tech,
+    u.response_time,
+    u.lines,
+    u.title,
+    u.words,
+    u.method,
+    u.scheme,
+    u.status_code,
+    u.content_type,
+    u.content_length,
+    u.page_type,
+    u.body_preview
+FROM urls u
+JOIN websites w ON u.url = w.url
+ON CONFLICT (website_id, path) DO NOTHING;
