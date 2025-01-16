b3226482903f073f15cdefa5a18450a4f723ee35
h3xit <85270067+h3xitsec@users.noreply.github.com>
2025-01-16 02:18:38 -0500

    use dnsx's recon option instead of just -a -cname

--- src/h3xrecon/plugins/plugins/resolve_domain.py
+++ src/h3xrecon/plugins/plugins/resolve_domain.py
@@ -24,7 +24,7 @@ class ResolveDomain(ReconPlugin):
 
     async def execute(self, params: Dict[str, Any], program_id: int = None, execution_id: str = None, db = None) -> AsyncGenerator[Dict[str, Any], None]:
         logger.debug(f"Running {self.name} on {params.get("target", {})}")
-        command = f"echo {params.get("target", {})} | ~/.pdtm/go/bin/dnsx -nc -resp -a -cname -silent -j | jq -Mc '{{host: .host,a_records: (.a // []),cnames: (.cname // [])}}'"
+        command = f"echo {params.get("target", {})} | ~/.pdtm/go/bin/dnsx -nc -resp -recon -silent -j #| jq -Mc '{{host: .host,a_records: (.a // []),cnames: (.cname // [])}}'"
         logger.debug(f"Running command: {command}")
         process = await self._create_subprocess_shell(command)
         
@@ -41,25 +41,29 @@ class ResolveDomain(ReconPlugin):
     async def process_output(self, output_msg: Dict[str, Any], db = None, qm = None) -> Dict[str, Any]:    
         try:
             #if await self.db.check_domain_regex_match(output_msg.get('output').get('host'), output_msg.get('program_id')):
-            if isinstance(output_msg.get('output').get('a_records'), list):
-                for ip in output_msg.get('output').get('a_records'):
-                    if isinstance(ip, str):
-                        try:
-                            await send_ip_data(qm=qm, data=ip, program_id=output_msg.get('program_id'), trigger_new_jobs=output_msg.get('trigger_new_jobs', True), execution_id=output_msg.get('execution_id'))
-                            logger.debug(f"Sent IP {ip} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
-                        except Exception as e:
-                            logger.error(f"Error processing IP {ip}: {str(e)}")
-                    else:
-                        logger.warning(f"Unexpected IP format: {ip}")
-            else:
-                logger.warning(f"Unexpected IP format: {output_msg.get('output').get('a_records')}")
+            for ip in output_msg.get('output').get('a'):
+                if isinstance(ip, str):
+                    try:
+                        await send_ip_data(qm=qm, data=ip, program_id=output_msg.get('program_id'), trigger_new_jobs=output_msg.get('trigger_new_jobs', True), execution_id=output_msg.get('execution_id'))
+                        logger.debug(f"Sent IP {ip} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
+                    except Exception as e:
+                        logger.error(f"Error processing IP {ip}: {str(e)}")
+                else:
+                    logger.warning(f"Unexpected IP format: {ip}")
+            if output_msg.get('output', {}).get('ns'):
+                for ns in output_msg.get('output', {}).get('ns', []):
+                    try:
+                        await send_domain_data(qm=qm, data=ns, program_id=output_msg.get('program_id'), trigger_new_jobs=output_msg.get('trigger_new_jobs', True), execution_id=output_msg.get('execution_id'))
+                        logger.debug(f"Sent NS {ns} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
+                    except Exception as e:
+                        logger.error(f"Error processing NS {ns}: {str(e)}")
             #if output_msg.get('output', {}).get('cnames'):
             await send_domain_data(
                 qm=qm,
                 data=output_msg.get('source', {}).get('params', {}).get('target'),
                 execution_id=output_msg.get('execution_id'),    
                 program_id=output_msg.get('program_id'),
-                attributes={"cnames": output_msg.get('output', {}).get('cnames'), "ips": output_msg.get('output', {}).get('a_records')},
+                attributes={"cnames": output_msg.get('output', {}).get('cnames'), "ips": output_msg.get('output', {}).get('a')},
                 trigger_new_jobs=output_msg.get('trigger_new_jobs', True)
             )
             logger.debug(f"Sent domain {output_msg.get('source', {}).get('params', {}).get('target')} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
