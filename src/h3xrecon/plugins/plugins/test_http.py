9762820de5740149ded73f38dde547665cfa7aa3
h3xit <85270067+h3xitsec@users.noreply.github.com>
2025-01-26 21:31:34 -0500

    fix empty service insertion

--- src/h3xrecon/plugins/plugins/test_http.py
+++ src/h3xrecon/plugins/plugins/test_http.py
@@ -87,69 +87,70 @@ class TestHTTP(ReconPlugin):
     async def process_output(self, output_msg: Dict[str, Any], db = None, qm = None) -> Dict[str, Any]:
         logger.debug(f"Incoming message:\nObject Type: {type(output_msg)} : {json.dumps(output_msg)}")
         # Parse full URL to extract host, port and path
-        logger.debug(f"Parsing URL: {output_msg.get("data", {}).get('url', '')}")
-        parsed_website_and_path = parse_url(output_msg.get("data", {}).get('url', ""))
-        if not parsed_website_and_path:
-            logger.error(f"Error parsing URL: {output_msg.get("data", {}).get('url', '')}")
-            return
-        website_msg = parsed_website_and_path.get('website')
-        website_msg['techs'] = output_msg.get("data", {}).get('tech', [])
-        website_msg['favicon_hash'] = output_msg.get("data", {}).get('favicon', "")
-        website_msg['favicon_url'] = output_msg.get("data", {}).get('favicon_url', "")
-        await send_website_data(qm, website_msg, output_msg.get('program_id', ""))
+        if output_msg.get("data", None):
+            logger.debug(f"Parsing URL: {output_msg.get("data", {}).get('url', '')}")
+            parsed_website_and_path = parse_url(output_msg.get("data", {}).get('url', ""))
+            if not parsed_website_and_path:
+                logger.error(f"Error parsing URL: {output_msg.get("data", {}).get('url', '')}")
+                return
+            website_msg = parsed_website_and_path.get('website')
+            website_msg['techs'] = output_msg.get("data", {}).get('tech', [])
+            website_msg['favicon_hash'] = output_msg.get("data", {}).get('favicon', "")
+            website_msg['favicon_url'] = output_msg.get("data", {}).get('favicon_url', "")
+            await send_website_data(qm, website_msg, output_msg.get('program_id', ""))
 
-        # Send website path data with full URL
-        website_path_msg = parsed_website_and_path.get('website_path')
-        website_path_msg['techs'] = output_msg.get("data", {}).get('tech', [])
-        website_path_msg['response_time'] = output_msg.get("data", {}).get('response_time')
-        website_path_msg['lines'] = output_msg.get("data", {}).get('lines')
-        website_path_msg['title'] = output_msg.get("data", {}).get('title')
-        website_path_msg['words'] = output_msg.get("data", {}).get('words')
-        website_path_msg['method'] = output_msg.get("data", {}).get('method')
-        website_path_msg['status_code'] = output_msg.get("data", {}).get('status_code')
-        website_path_msg['content_type'] = output_msg.get("data", {}).get('content_type')
-        website_path_msg['content_length'] = output_msg.get("data", {}).get('content_length')
-        website_path_msg['chain_status_codes'] = output_msg.get("data", {}).get('chain_status_codes')
-        website_path_msg['page_type'] = output_msg.get("data", {}).get('page_type')
-        website_path_msg['body_preview'] = output_msg.get("data", {}).get('body_preview')
-        website_path_msg['resp_header_hash'] = output_msg.get("data", {}).get('hash', {}).get('header_sha256', "")
-        website_path_msg['resp_body_hash'] = output_msg.get("data", {}).get('hash', {}).get('body_sha256', "")
-        await send_website_path_data(qm, website_path_msg, output_msg.get('program_id', ""))
-        # Extract domain names from the httpx output to send to data worker
-        # All domains specified in response body, all FQDNs and all subject alternative names
-        domains_to_add = (output_msg.get("data", {}).get('body_domains', []) + 
-                            output_msg.get("data", {}).get('body_fqdn', []) + 
-                            output_msg.get("data", {}).get('tls', {}).get('subject_an', []))
-        
-        logger.debug(f"Domains to add: {domains_to_add}")
-        if len(domains_to_add) > 0:
-            for domain in domains_to_add:
-                if is_valid_hostname(domain):
-                    logger.debug(f"Sending domain data for {domain}")
-                    await send_domain_data(qm=qm, data=domain, program_id=output_msg.get('program_id'), execution_id=output_msg.get('execution_id', ""), trigger_new_jobs=output_msg.get('trigger_new_jobs', True))
+            # Send website path data with full URL
+            website_path_msg = parsed_website_and_path.get('website_path')
+            website_path_msg['techs'] = output_msg.get("data", {}).get('tech', [])
+            website_path_msg['response_time'] = output_msg.get("data", {}).get('response_time')
+            website_path_msg['lines'] = output_msg.get("data", {}).get('lines')
+            website_path_msg['title'] = output_msg.get("data", {}).get('title')
+            website_path_msg['words'] = output_msg.get("data", {}).get('words')
+            website_path_msg['method'] = output_msg.get("data", {}).get('method')
+            website_path_msg['status_code'] = output_msg.get("data", {}).get('status_code')
+            website_path_msg['content_type'] = output_msg.get("data", {}).get('content_type')
+            website_path_msg['content_length'] = output_msg.get("data", {}).get('content_length')
+            website_path_msg['chain_status_codes'] = output_msg.get("data", {}).get('chain_status_codes')
+            website_path_msg['page_type'] = output_msg.get("data", {}).get('page_type')
+            website_path_msg['body_preview'] = output_msg.get("data", {}).get('body_preview')
+            website_path_msg['resp_header_hash'] = output_msg.get("data", {}).get('hash', {}).get('header_sha256', "")
+            website_path_msg['resp_body_hash'] = output_msg.get("data", {}).get('hash', {}).get('body_sha256', "")
+            await send_website_path_data(qm, website_path_msg, output_msg.get('program_id', ""))
+            # Extract domain names from the httpx output to send to data worker
+            # All domains specified in response body, all FQDNs and all subject alternative names
+            domains_to_add = (output_msg.get("data", {}).get('body_domains', []) + 
+                                output_msg.get("data", {}).get('body_fqdn', []) + 
+                                output_msg.get("data", {}).get('tls', {}).get('subject_an', []))
+            
+            logger.debug(f"Domains to add: {domains_to_add}")
+            if len(domains_to_add) > 0:
+                for domain in domains_to_add:
+                    if is_valid_hostname(domain):
+                        logger.debug(f"Sending domain data for {domain}")
+                        await send_domain_data(qm=qm, data=domain, program_id=output_msg.get('program_id'), execution_id=output_msg.get('execution_id', ""), trigger_new_jobs=output_msg.get('trigger_new_jobs', True))
 
-        # Parse and send service data
-        await send_service_data(qm=qm, data={
-                "ip": output_msg.get("data").get('host'), 
-                "port": int(output_msg.get("data").get('port')), 
-                "protocol": "tcp",
-                "scheme": output_msg.get("data").get('scheme', "")
-            }, program_id=output_msg.get('program_id'))
+            # Parse and send service data
+            await send_service_data(qm=qm, data={
+                    "ip": output_msg.get("data").get('host'), 
+                    "port": int(output_msg.get("data").get('port')), 
+                    "protocol": "tcp",
+                    "scheme": output_msg.get("data").get('scheme', "")
+                }, program_id=output_msg.get('program_id'))
 
-        # If a HTTPS response is parsed, send certificate data
-        if output_msg.get("data").get('tls', {}).get('subject_dn', "") != "":
-            await send_certificate_data(qm=qm,data={
-                "url": output_msg.get("data", {}).get('url', ""),
-                "cert": {   
-                    "subject_dn": output_msg.get("data").get('tls', {}).get('subject_dn', ""),
-                    "subject_cn": output_msg.get("data").get('tls', {}).get('subject_cn', ""),
-                    "subject_an": output_msg.get("data").get('tls', {}).get('subject_an', ""),
-                    "valid_date": output_msg.get("data").get('tls', {}).get('not_before', ""),
-                    "expiry_date": output_msg.get("data").get('tls', {}).get('not_after', ""),
-                    "issuer_dn": output_msg.get("data").get('tls', {}).get('issuer_dn', ""),
-                    "issuer_cn": output_msg.get("data").get('tls', {}).get('issuer_cn', ""),
-                    "issuer_org": output_msg.get("data").get('tls', {}).get('issuer_org', ""),
-                    "serial": output_msg.get("data").get('tls', {}).get('serial', ""),
-                    "fingerprint_hash": output_msg.get("data").get('tls', {}).get('fingerprint_hash', {}).get('sha256', "")
-                }
-            }, program_id=output_msg.get('program_id'))
+            # If a HTTPS response is parsed, send certificate data
+            if output_msg.get("data").get('tls', {}).get('subject_dn', "") != "":
+                await send_certificate_data(qm=qm,data={
+                    "url": output_msg.get("data", {}).get('url', ""),
+                    "cert": {   
+                        "subject_dn": output_msg.get("data").get('tls', {}).get('subject_dn', ""),
+                        "subject_cn": output_msg.get("data").get('tls', {}).get('subject_cn', ""),
+                        "subject_an": output_msg.get("data").get('tls', {}).get('subject_an', ""),
+                        "valid_date": output_msg.get("data").get('tls', {}).get('not_before', ""),
+                        "expiry_date": output_msg.get("data").get('tls', {}).get('not_after', ""),
+                        "issuer_dn": output_msg.get("data").get('tls', {}).get('issuer_dn', ""),
+                        "issuer_cn": output_msg.get("data").get('tls', {}).get('issuer_cn', ""),
+                        "issuer_org": output_msg.get("data").get('tls', {}).get('issuer_org', ""),
+                        "serial": output_msg.get("data").get('tls', {}).get('serial', ""),
+                        "fingerprint_hash": output_msg.get("data").get('tls', {}).get('fingerprint_hash', {}).get('sha256', "")
+                    }
+                }, program_id=output_msg.get('program_id'))
