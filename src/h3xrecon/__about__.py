2bd6d4f9a2bbc9dbafe92e1b9442035f54a2c7c4
h3xit <85270067+h3xitsec@users.noreply.github.com>
2025-02-13 00:53:38 -0500

    fix dnsx output processing to send domain after ips

--- src/h3xrecon/__about__.py
+++ src/h3xrecon/__about__.py
@@ -2,4 +2,4 @@
 #
 # SPDX-License-Identifier: MIT
 
-__version__ = "0.10.0.dev1"
+__version__ = "0.10.0.dev3"
--- src/h3xrecon/plugins/plugins/dnsx.py
+++ src/h3xrecon/plugins/plugins/dnsx.py
@@ -37,7 +37,7 @@ class DnsxPlugin(ReconPlugin):
         # Select 3 random resolvers from the file
         with open(RESOLVERS_FILE, 'r') as file:
             resolvers = file.read().splitlines()
-        random_resolvers = random.sample(resolvers, 3)
+        random_resolvers = random.sample(resolvers, 5)
         random_resolvers_str = ",".join(random_resolvers)
         
         # Run dnsx
@@ -51,10 +51,14 @@ class DnsxPlugin(ReconPlugin):
         for r in dnsx_results:
             try:
                 json_data = json.loads(r)
-                logger.debug(f"Parsed output: {json_data}")
-                results.append(json_data)
+                if json_data.get('status_code') == "NOERROR":
+                    logger.debug(f"Parsed output: {json_data}")
+                    results.append(json_data)
             except json.JSONDecodeError as e:
                 logger.error(f"Failed to parse JSON output: {e}")
+        
+        if len(results) == 0:
+            return
         
         # Yield the results along with the target and parent domain information
         message = {
@@ -67,6 +71,37 @@ class DnsxPlugin(ReconPlugin):
     async def process_output(self, output_msg: Dict[str, Any], db = None, qm = None) -> Dict[str, Any]:    
         try:
             dnsx_results = output_msg.get('data', {}).get('dnsx_results', [])[0]
+            # Process DNS entries
+            for r in dnsx_results.get('all', []):
+                parsed_record = parse_dns_record(r)
+                if parsed_record:
+                    parsed_record['target_domain'] = output_msg.get('source', {}).get('params',{}).get('target')
+                    if parsed_record.get('target_domain') in parsed_record.get('hostname'):
+                        await send_dns_data(qm=qm, 
+                                            data=parsed_record,
+                                            program_id=output_msg.get('program_id'),
+                                            trigger_new_jobs=output_msg.get('trigger_new_jobs', False),
+                                            execution_id=output_msg.get('execution_id'),
+                                            response_id = None
+                                        )
+                        logger.debug(f"Sent DNS record {parsed_record} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
+            
+            # Process IPs from A records
+            for ip in dnsx_results.get('a', []):
+                if isinstance(ip, str):
+                    try:
+                        await send_ip_data(qm=qm, 
+                                           data=ip, 
+                                           program_id=output_msg.get('program_id'), 
+                                           trigger_new_jobs=output_msg.get('trigger_new_jobs', True), 
+                                           execution_id=output_msg.get('execution_id'), 
+                                           response_id=None)
+                        
+                        logger.debug(f"Sent IP {ip} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
+                    except Exception as e:
+                        logger.error(f"Error processing IP {ip}: {str(e)}")
+                else:
+                    logger.warning(f"Unexpected IP format: {ip}")
             # Set the domain attributes
             dom_attr = {
                 "cnames": dnsx_results.get('cnames', []), 
@@ -85,37 +120,7 @@ class DnsxPlugin(ReconPlugin):
                     response_id=None
                 )
                 logger.debug(f"Sent domain {output_msg.get('source', {}).get('params', {}).get('target')} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
-            # Process DNS entries
-            for r in dnsx_results.get('all', []):
-                parsed_record = parse_dns_record(r)
-                if parsed_record:
-                    parsed_record['target_domain'] = output_msg.get('source', {}).get('params',{}).get('target')
-                    await send_dns_data(qm=qm, 
-                                        data=parsed_record,
-                                        program_id=output_msg.get('program_id'),
-                                        trigger_new_jobs=output_msg.get('trigger_new_jobs', False),
-                                        execution_id=output_msg.get('execution_id'),
-                                        response_id = None
-                                    )
-                    logger.debug(f"Sent DNS record {parsed_record} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
-            
-            # Process IPs from A records
-            for ip in dnsx_results.get('a', []):
-                if isinstance(ip, str):
-                    try:
-                        await send_ip_data(qm=qm, 
-                                           data=ip, 
-                                           program_id=output_msg.get('program_id'), 
-                                           trigger_new_jobs=output_msg.get('trigger_new_jobs', True), 
-                                           execution_id=output_msg.get('execution_id'), 
-                                           response_id=None)
-                        
-                        logger.debug(f"Sent IP {ip} to data processor queue for domain {output_msg.get('source', {}).get('params',{}).get('target')}")
-                    except Exception as e:
-                        logger.error(f"Error processing IP {ip}: {str(e)}")
-                else:
-                    logger.warning(f"Unexpected IP format: {ip}")
-            
+
             # Process NS records
             if dnsx_results.get('ns'):
                 for ns in dnsx_results.get('ns', []):
