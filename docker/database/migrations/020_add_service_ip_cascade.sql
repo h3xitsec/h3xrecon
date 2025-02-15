233cb77a9e2ce7365442e65393ec5dbad01f14e5
h3xit <85270067+h3xitsec@users.noreply.github.com>
2025-02-14 21:11:22 -0500

    Revert "202 add more cloudcdnwaf provider to ip detection (#205)"
    This reverts commit 32f54cd051e64914d85f8a9275bc764be30fdbdc.

--- docker/database/migrations/020_add_service_ip_cascade.sql
+++
@@ -1,9 +0,0 @@
--- First, drop the existing foreign key constraint
-ALTER TABLE services DROP CONSTRAINT IF EXISTS services_ip_fkey;
-
--- Add the new foreign key constraint with ON DELETE CASCADE
-ALTER TABLE services
-    ADD CONSTRAINT services_ip_fkey
-    FOREIGN KEY (ip)
-    REFERENCES ips(id)
-    ON DELETE CASCADE;
